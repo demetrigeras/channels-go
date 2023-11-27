aabdcc211325d549bedbc4ce3a05bdc3cafc391c
demetrigeras <demetrijgeras@gmail.com>
2023-11-27 12:56:16 -0500

    test

--- main.go
+++ main.go
@@ -15,17 +15,24 @@ func main() {
 		"http://amazon.com",
 		"http://nike.com",
 	}
-	for _, link := range links {
-		go checkLink(link)
-	}
 
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is down!")
+		c <- "Might be down I think"
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "Yep its up"
 }
